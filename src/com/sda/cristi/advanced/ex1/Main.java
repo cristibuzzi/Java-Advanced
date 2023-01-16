bb9b80771c470d43e95fdc161c4be869a057b9f9
Cristi <buzzi_cristi@yahoo.com>
2023-01-16 20:04:07 +0200

    Add git package

--- src/com/sda/cristi/advanced/ex1/Main.java
+++
@@ -1,9 +0,0 @@
-package com.sda.cristi.advanced.ex1;
-/**
- *
- */
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
--- src/com/sda/cristi/advanced/ex1/Thread1.java
+++
@@ -1,4 +0,0 @@
-package com.sda.cristi.advanced.ex1;
-
-public class Thread1 extends Thread{
-}
